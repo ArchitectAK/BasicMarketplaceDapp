fd5888a4d76b75a9aa7b0623331bc11ae998a7a0
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-02-24 20:16:14 +0800

    Add js script to display data from sample json file

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,34 @@
 App = {
   init: async function () {
-    console.log('Initializing')
+    console.log('Initializing');
+    $.getJSON("../sampleData.json", function (data) {
+      console.log(data);
+      const allItemsDiv = $("#all-items");
+      const itemTemplate = $("#item-template");
+      for (let i = 0; i < data.length; i++) {
+        itemTemplate.find(".item-name").text(data[i].itemName)
+        itemTemplate.find(".item-creator").text(data[i].itemCreator)
+        itemTemplate.find(".item-owner").text(data[i].itemOwner)
+        itemTemplate.find(".asking-price").text(data[i].askingPrice)
+        itemTemplate.find(".item-status").text(data[i].isSold ? 'sold' : 'available')
+
+        if(data[i].isSold)
+        {
+          itemTemplate.find(".btn-buy").hide();
+        } else {
+          itemTemplate.find(".btn-buy").show();
+        }
+
+        allItemsDiv.append(itemTemplate.html());
+      }
+    })
+
   },
-  
 }
 
-$(function(){
+$(function () {
 
-  $(window).load(function(){
+  $(window).load(function () {
     App.init()
   })
 
