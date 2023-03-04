9fbb5e55e19c7b4ec4c39234622c930feb80b009
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-03-04 15:20:48 +0800

    Add unit test for addProduct function

--- test/BasicMarketplace.js
+++ test/BasicMarketplace.js
@@ -1,4 +1,4 @@
-const { except } = require("chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { describe, it } = require("mocha");
 
@@ -8,7 +8,18 @@ describe("BasicMarketplace", function () {
     const contract = await Contract.deploy();
     await contract.deployed()
 
-    except(await contract.numProduct()).to.equal(1);
+    expect(await contract.numProduct()).to.equal(1);
+  })
+
+  it("Should create a new product", async function () {
+    const Contract = await ethers.getContractFactory("BasicMarketplace");
+    const contract = await Contract.deploy();
+    await contract.deployed()
+
+    const addProductTx = await contract.addProduct("test product", 100);
+    await addProductTx.wait();
+
+    expect(await contract.numProduct()).to.equal(2);
   })
 
 })
